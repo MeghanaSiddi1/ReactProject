ba9fd2e0bf361b3860d1a63c90e24d48a78aa1b7
Meghana <siddimeghana3@gmail.com>
2024-09-25 14:03:34 -0400

    Episode 03-5th

--- App.js
+++ App.js
@@ -3,23 +3,27 @@ import ReactDOM from "react-dom/client";
 
 //JSX (It is ranspiled before it reaches the JS) - With the help of Babel which is dependency of Parcel
 
-// JSX => Babel conversts to React.CreateElement => Which is React Element. React Elemnet is nothing but JS object. render() renders that JS object to actual HTML element. 
+// JSX => Babel conversts to React.CreateElement => Which is React Element. React Elemnet is nothing but JS object. render() renders that JS object to actual HTML element.
 
-
-const heading = (<h1 id="heading" tabIndex="5" className="heading">Namaste React using JSX🚀</h1>);
-console.log(heading);
+const Tile = () => (
+  <h1 id="title" tabIndex="5" className="title">
+    Namaste React using JSX🚀
+  </h1>
+);
 
 // React Functional Component
 
-const HeadingComponent = () => {
-  return <h1 className="heading">Namaste React Functional Component</h1>
-}
-
-// const HeadingComponent2 = () => (<h1 className="heading">Namaste React Functional Component</h1>);
+const HeadingComponent = () => (
+  <div id="container">
+    <Tile/>
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>  
+);
 
 // HeadingComponent and HeadingComponent2 compoents are both same but change in syntax.
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); // Here heading object replaces whatever is there in the root div in HTML
+// root.render(heading); // Here it is for JSX/ React elemt.Here heading object replaces whatever is there in the root div in HTML
+
+root.render(<HeadingComponent />);
