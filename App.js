733b9f7ab4ce8e135c54ce7763fab6720f023d95
Meghana <siddimeghana3@gmail.com>
2024-09-25 13:26:03 -0400

    Episode 03-3rd

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Welcome World from React!");
-// console.log(heading); // object
-// const root= ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading); // convert object to h1 tag
-
-const heading = React.createElement("h1",{id:"heading"},"Namaste React 🚀");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
-console.log(heading);
-
 //JSX (It is ranspiled before it reaches the JS) - With the help of Babel which is dependency of Parcel
 
 // JSX => Babel conversts to React.CreateElement => Which is React Element. React Elemnet is nothing but JS object. render() renders that JS object to actual HTML element. 
 
-const JSXheading = (<h1 id="heading" tabIndex="5" className="heading">Namaste React using JSX🚀</h1>);
-console.log(JSXheading);
 
-root.render(JSXheading); // Here parent object replaces whatever is there in the root div in HTML
+const heading = (<h1 id="heading" tabIndex="5" className="heading">Namaste React using JSX🚀</h1>);
+console.log(heading);
+
+// React Component
+// Class Based Compoent - OLD
+// Functional Component - New
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); // Here heading object replaces whatever is there in the root div in HTML
