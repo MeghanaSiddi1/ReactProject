7add5f05f36a129f554d762b855f9009c5aa4bec
Meghana <siddimeghana3@gmail.com>
2024-09-26 11:35:33 -0400

    Episode 03-9th - diff ways of calling a fn component

--- App.js
+++ App.js
@@ -19,9 +19,9 @@ const Title = () =>(
 
 const HeadingComponent = () => (
   <div id="container">
-    {Title()}
+    {/* {Title()} */}
     <Title/>
-    <Title></Title>  
+    {/* <Title></Title>   */}
     <h1 className="heading">Namaste React Functional Component</h1>   
   </div>
 );
