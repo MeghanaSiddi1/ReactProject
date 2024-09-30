5c21b221ab0b88a0a4d1dc0b18fa2e49694fb45c
Meghana <siddimeghana3@gmail.com>
2024-09-30 00:10:58 -0500

    episode-05-useState hook introduction

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,45 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
+// not using key (not recommended) << index as key <<< unique id as key
+
+const Body = () => {
+    // const arr=useState(resList);
+    // const [listOfRestaurants, setListOfRestaurants] = arr;
+    // The above one also works
+
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  console.log('listOfRes',listOfRestaurants);
+
+//   const listOfRestaurants = arr[0];
+//   const setListOfRestaurants = arr[1];
+//  But we are destructuring on the fly instead of using this
 
 
-// not using key (not recommended) << index as key <<< unique id as index
-
-
-
-const Body = () =>{
-    return (<div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">      
-        {
-          resList.map((restaurant)=>(<RestaurantCard key={restaurant.info.id} resData={restaurant}/>))
-        }  
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (rest) => rest.info.avgRating > 4.6
+            );
+            console.log('updated', filteredList);
+             setListOfRestaurants(filteredList);
+           // setListOfRestaurants(prev => prev.filter(rest => rest.info.avgRating > 4.6));
+          }}
+        >
+          Top rated Restaurants
+        </button>
       </div>
-    </div>)
-  }
-  export default Body;
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
