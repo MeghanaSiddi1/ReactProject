b98cd2b8093e774d02ed6f82050c161a391d298b
Meghana <siddimeghana3@gmail.com>
2024-09-30 20:22:01 -0500

    episode-07-routing-introduction but without child routes

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () =>{
+    return(
+        <div>
+            <h1>This is Food Delivering App</h1>
+            <h2>Designed by Meghana</h2>
+        </div>        
+    )
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact=()=>{
+    return(
+        <div>
+            <h1>Please contact me on siddimeghana3@gmail.com</h1>
+        </div>
+    )
+};
+
+export default Contact;
